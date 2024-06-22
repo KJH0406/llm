5303cc3e52d70c798676ffea45f2719c38e05ca7
김장호 <rlawkdgh0406@naver.com>
2024-06-22 17:22:21 +0900

    feat: chain 문법 사용하여 RAG 구현(응답 스트리밍 미구현)

--- pages/rag_gpt.py
+++ pages/rag_gpt.py
@@ -1,9 +1,12 @@
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
-from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
 # 페이지 인터페이스 설정
@@ -13,6 +16,7 @@ st.title("RAG")
 with st.sidebar:
     file = st.file_uploader("파일을 업로드 해주세요.", type=["pdf", "txt", "docx", "md"]) # 업로드 가능한 파일 확장자 지정
 
+# 파일 임베딩하여 벡터 스토어에 저장하고 검색 증강 실행
 @st.cache_data(show_spinner="임베딩 처리 중입니다.")
 def embed_file(file):
     file_content = file.read() # 업로드된 파일의 내용을 읽어서 file_content 변수에 저장
@@ -49,14 +53,24 @@ def print_messages():
     for message in st.session_state["messages"]:
         send_message(message["message"], message["role"], save=False)
 
+# docs(추출된 문서)리스트 안에서 각 추출된 문서 덩어리를 가져오고 해당 문서 덩어리의 page_content부분을 가져와서 두줄의 공백으로 join
+def formatting_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+# LLM 모델 설정
+llm = ChatOpenAI(
+    model_name="gpt-4o",
+    temperature=0,
+)
+
 # 프롬프트 템플릿 생성
-template = ChatPromptTemplate.from_messages([
+prompt = ChatPromptTemplate.from_messages([
     # 시스템 프롬프트 입력
     ("system",
     """
-    You are an assistant for question-answering tasks. 
-    Use the following pieces of retrieved context to answer the question.
-    If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+    Answer the question using ONLY the following context.
+    If you don't know the answer just say you don't know. DON'T make anything up.
 
     Context: {context}
     
@@ -82,14 +96,15 @@ if file:
     # 사용자가 메시지 입력 시 실행
     if message:
         send_message(message, "human") # 메시지 생성 함수 실행
-        docs = retriever.invoke(message) # 사용자가 입력한 질문을 통해 문서에서 관련된 부분 추출(page_content)
-        docs = "\n\n".join(document.page_content for document in docs ) # docs(추출된 문서)리스트 안에서 각 추출된 문서 덩어리를 가져오고 해당 문서 덩어리의 page_content부분을 가져와서 두줄의 공백으로 join
+        
+        chain = {
+            "context" : retriever | RunnableLambda(formatting_docs),  # docs = retriever.invoke(message) and formatting_docs
+            "question" : RunnablePassthrough()
+        } | prompt | llm
 
-        # 위에서 생성한 프롬프트 템플릿 사용하여 최종 프롬프트 생성
-        prompt = template.format_messages(context=docs, question=message)
-        st.write(prompt)
-
-        # 여기서 llm 모델 실햏하는 것까지 들어가면 체인없이 수동으로 RAG 과정 실행하는 것
+        # 체인 실행 및 응답 생성
+        response = chain.invoke(message).content
+        send_message(response, "ai")
 
 else:
     # 처음 파일 업로드 되지 않았을 시 실행
