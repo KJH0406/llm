5f195ede2bb1ad7faa126f180c3d85c773e9f723
김장호 <rlawkdgh0406@naver.com>
2024-06-22 12:49:34 +0900

    feat: GPT API APP 메인 화면 생성

---
+++ home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="📚",
+)
+
+st.markdown(
+"""            
+GPT API APP!
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
