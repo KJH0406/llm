b0c698f30cf7e945a093c447105f2548a1fa7402
김장호 <rlawkdgh0406@naver.com>
2024-06-23 13:40:48 +0900

    feat: 각 체인을 연결하여 원하는 출력 생성 1. 문서에서 모든 내용 추출 2. 추출한 모든 내용을 기반으로 Q&A 생성 3. 생성된 Q&A를 json 형식으로 변환

--- pages/quiz_gpt.py
+++ pages/quiz_gpt.py
@@ -8,7 +8,17 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.retrievers import WikipediaRetriever 
+from langchain.schema import BaseOutputParser
 import streamlit as st
+import json
+
+class JsonOutputParser(BaseOutputParser):
+    # 프롬프트 템플릿 json 안쪽 코드 json 파일로 로드
+    def parse(self, text):  
+        text = text.replace("```", "").replace("json", "")
+        return json.loads(text)
+
+output_parser = JsonOutputParser()
 
 st.set_page_config(
     page_title="Output parser",
@@ -26,59 +36,7 @@ llm = ChatOpenAI(
     ],
 )
 
-# 파일을 읽고 텍스트 분할
-@st.cache_data(show_spinner="파일을 처리하는 중입니다...") # 데이터 캐싱
-def split_file(file):
-    # 업로드된 파일 처리
-    file_content = file.read() 
-    file_path = f"./.cache/quiz_files/{file.name}" 
-    with open(file_path, "wb") as f: 
-        f.write(file_content) 
-        # 텍스트 스플리터 설정
-        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        # separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        )
-        loader = UnstructuredFileLoader(file_path)
-        docs = loader.load_and_split(text_splitter=splitter)
-        return docs
-
-
-# 퀴즈 생성 방식 설정
-with st.sidebar:
-    docs = None
-    choice = st.selectbox("방식을 선택해주세요.", (
-        "내 파일 사용", "위키피디아 사용",
-    ))
-    if choice == "내 파일 사용":
-        file = st.file_uploader("파일을 업로드해주세요.", type=["pdf, txt", "docx", "md"])
-        if file:
-            docs = split_file(file)
-    else:
-        topic = st.text_input("주제를 입력해주세요.")
-        if topic:
-            retriever = WikipediaRetriever(top_k_results=5)
-            with st.status("관련 내용을 검색 중입니다..."):
-                docs = retriever.get_relevant_documents(topic)
-
-# 문서 리스트 포맷팅
-def formatting_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
-
-# 초기 화면 설정(파일 업로드 되지 않았을 때)
-if not docs:
-    st.markdown(
-        """
-        ‼️ 퀴즈 생성 방법(택 1)
-        1. 파일을 업로드한다. 
-        2. 위키피디아에 주제를 검색한다.
-        """
-    )
-else:
-    st.write(docs)
-
-    prompt = ChatPromptTemplate.from_messages(
+question_prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
@@ -116,11 +74,191 @@ else:
     ) 
 
 
-    chain = {"context": formatting_docs}|prompt | llm
+def formatting_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
 
-    start = st.button("생성하기")
 
-    if start:
-        response = chain.invoke(docs).content
-        if response:
-            st.write(response)
+question_chain = {"context": formatting_docs}|question_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a powerful formatting algorithm.
+     
+            You format exam questions into JSON format.
+            Answers with (o) are the correct ones.
+
+            Questions and answers must be written in Korean.
+            
+            Example Input:
+            Question: What is the color of the ocean?
+            Answers: Red|Yellow|Green|Blue(o)
+                
+            Question: What is the capital or Georgia?
+            Answers: Baku|Tbilisi(o)|Manila|Beirut
+                
+            Question: When was Avatar released?
+            Answers: 2007|2001|2009(o)|1998
+                
+            Question: Who was Julius Caesar?
+            Answers: A Roman Emperor(o)|Painter|Actor|Model
+            
+            
+            Example Output:
+            
+            ```json
+            {{ "questions": [
+                    {{
+                        "question": "What is the color of the ocean?",
+                        "answers": [
+                                {{
+                                    "answer": "Red",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Yellow",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Green",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Blue",
+                                    "correct": true
+                                }},
+                        ]
+                    }},
+                                {{
+                        "question": "What is the capital or Georgia?",
+                        "answers": [
+                                {{
+                                    "answer": "Baku",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Tbilisi",
+                                    "correct": true
+                                }},
+                                {{
+                                    "answer": "Manila",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Beirut",
+                                    "correct": false
+                                }},
+                        ]
+                    }},
+                                {{
+                        "question": "When was Avatar released?",
+                        "answers": [
+                                {{
+                                    "answer": "2007",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "2001",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "2009",
+                                    "correct": true
+                                }},
+                                {{
+                                    "answer": "1998",
+                                    "correct": false
+                                }},
+                        ]
+                    }},
+                    {{
+                        "question": "Who was Julius Caesar?",
+                        "answers": [
+                                {{
+                                    "answer": "A Roman Emperor",
+                                    "correct": true
+                                }},
+                                {{
+                                    "answer": "Painter",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Actor",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Model",
+                                    "correct": false
+                                }},
+                        ]
+                    }}
+                ]
+            }}
+            ```
+             Your turn!
+             Input: {context}
+            """
+            
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
+@st.cache_data(show_spinner="Q&A를 작성 중입니다...")
+def run_QnA_chain(_docs, topic):
+    chain = {"context": question_chain} | formatting_chain | output_parser
+    response = chain.invoke(_docs)
+    return response
+
+@st.cache_data(show_spinner="Q&A를 작성 중입니다...")
+def wiki_search(term):
+    retriever = WikipediaRetriever(top_k_results=5)
+    docs = retriever.get_relevant_documents(term)
+    return docs
+
+@st.cache_data(show_spinner="파일을 처리하는 중입니다...") 
+def split_file(file):
+    file_content = file.read() 
+    file_path = f"./.cache/quiz_files/{file.name}" 
+    with open(file_path, "wb") as f: 
+        f.write(file_content) 
+        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+        )
+        loader = UnstructuredFileLoader(file_path)
+        docs = loader.load_and_split(text_splitter=splitter)
+        return docs
+
+
+# 퀴즈 생성 방식 설정
+with st.sidebar:
+    docs = None
+    choice = st.selectbox("방식을 선택해주세요.", (
+        "내 파일 사용", "위키피디아 사용",
+    ))
+    if choice == "내 파일 사용":
+        file = st.file_uploader("파일을 업로드해주세요.", type=["pdf, txt", "docx", "md"])
+        if file:
+            docs = split_file(file)
+    else:
+        topic = st.text_input("주제를 입력해주세요.")
+        if topic:
+            docs = wiki_search(topic)
+            
+
+# 초기 화면 설정(파일 업로드 되지 않았을 때)
+if not docs:
+    st.markdown(
+        """
+        ‼️ 퀴즈 생성 방법(택 1)
+        1. 파일을 업로드한다. 
+        2. 위키피디아에 주제를 검색한다.
+        """
+    )
+else:
+    response = run_QnA_chain(docs, topic if topic else file.name)
+    st.write(response)
