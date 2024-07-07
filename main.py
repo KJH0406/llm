f7c7e02835fcdda99e7f75f24f89a7cb21097359
김장호 <rlawkdgh0406@naver.com>
2024-07-07 15:25:14 +0900

    feat: Authentication 추가

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
-from pydantic import BaseModel,Field
+from typing import Any, Dict
+from fastapi import Body, FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
 
 # 서버 구축
 app = FastAPI(
     title="Jang Ho Maximus Quote Giver", # 이름
     description="Get a real quote said by Jang Ho Maximus himself.", # 설명
     servers=[
-        {"url":"https://sur-chain-sentence-assured.trycloudflare.com"} # 제공될 서버 URL을 설정
+        {"url":"https://blond-nursery-touring-rd.trycloudflare.com"} # 제공될 서버 URL을 설정
     ]
 )
 
@@ -34,8 +36,39 @@ class QuoteArgsSchema(BaseModel):
     response_model=QuoteArgsSchema, # 응답 모델로 사용할 데이터 스키마(명언: 문자열 타입, 연도: 정수 타입) 설정
     )
 # '/quote' 엔드포인트로 GET 요청이 들어왔을 때 실행
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "삶은 짧다...먹고 싶은거 다먹어라..",
         "year": 2024,
     }
+
+# 임의의 유저 토큰 생성
+user_token_db = {
+    "ABCDEF": "jangho"
+}
+
+@app.get(
+    "/authorize",
+    response_class=HTMLResponse,
+)
+def handle_authorize(client_id: str, redirect_uri: str, state:str):
+    return f"""
+    <html>
+        <head>
+            <title>Quote Log In</title>
+        </head>
+        <body>
+            <h1>Log Into Quote</h1>
+            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize</a>
+        </body>
+    </html>
+    """
+
+@app.post("/token")
+def handle_token(code=Form(...)):
+    return {
+        "access_token": user_token_db[code],
+    }
+
+    
